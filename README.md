18091a1f333cd050495bdf38fb69436b6403f065
Patrick Phua <reach.patrickphua@gmail.com>
2024-02-26 16:44:10 +0800

    moving config file, update README

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# ETH-Sweeper
+```markdown
+# Ethereum Wallet Sweeper
+
+This Node.js script is designed to programmatically sweep Ethereum (ETH) from a compromised wallet in order to prevent attackers from moving assets. It provides a way to secure funds in case of a compromised private key or wallet.
+
+## Prerequisites
+
+Before running this script, ensure you have the following:
+
+- Node.js installed on your system.
+- An Ethereum wallet with the compromised private key.
+- An API key from [Infura.io](https://docs.infura.io/dashboard/create-api)
+- NPM dependencies installed. (Install them by running `npm install` in the project directory.)
+
+## Setup
+
+1. Clone or download this repository to your local machine.
+2. Navigate to the project directory in your terminal.
+3. Run `npm install` to install dependencies.
+4. Update the `config.js` file with your Ethereum wallet address, private key or wallet file, and Ethereum node URL.
+
+## Usage
+
+To sweep ETH from the compromised wallet, follow these steps:
+
+1. Ensure your Ethereum node is running and accessible.
+2. Navigate to the project directory in your terminal.
+3. Run the script by executing `node sweep.js`.
+4. Once the sweep is completed, the script will display the transaction hash for the sweep transaction.
+
+## Configuration
+
+In the `config.js` file, you need to provide the following information:
+
+- `ETHEREUM_NETWORK`: mainnet, sepolia
+- `CHAIN_ID`: for mainnet (1), for sepolia (11155111)
+- `INFURA_API_KEY`: API key from [Infura.io](https://docs.infura.io/dashboard/create-api)
+- `WALLET_TO_SWEEP_PRIVATE_KEY`: The private key or path to the wallet file of the compromised wallet.
+- `WALLET_DESTINATION`: The address of the destination wallet where funds will be swept into.
+- `MIN_ETH_SWEEP`: The balance threshold in which the script will run to sweep the funds.
+- `TX_MAX_GWEI`: The gas fees to be set for the transaction.
+- `TX_MAX_PRIORITY_FEE`: The priority fees to be set for the transaction.
+
+## Security Considerations
+
+- Ensure that your private key or wallet file is kept secure and not exposed to unauthorized parties.
+- Be cautious when running this script, as it will initiate a transaction on the Ethereum network.
+- Always verify the code and dependencies before running any script that involves your private keys or transactions.
+
+## Disclaimer
+
+This script is provided as-is without any warranties. Use it at your own risk. The authors of this script are not responsible for any loss of funds or damages resulting from the use of this script.
+
+## License
+
+[MIT License](LICENSE)
+```
--- sweep.js
+++ sweep.js
@@ -1,24 +1,26 @@
 const { Web3 } = require("web3");
+var CONFIG = require('./config.json');
+
 async function main() {
   // Configuring the connection to an Ethereum node
-  const network = process.env.ETHEREUM_NETWORK;
+  const network = CONFIG.ETHEREUM_NETWORK;
   const web3 = new Web3(
     new Web3.providers.HttpProvider(
-      `https://${network}.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      `https://${network}.infura.io/v3/${CONFIG.INFURA_API_KEY}`,
     ),
   );
 
   // Creating a signing account from a private key
   const signer = web3.eth.accounts.privateKeyToAccount(
-    process.env.WALLET_TO_SWEEP_PRIVATE_KEY,
+    CONFIG.WALLET_TO_SWEEP_PRIVATE_KEY,
   );
   web3.eth.accounts.wallet.add(signer);
 
   // Check Balance
   var balance = await web3.eth.getBalance(signer.address);
-  const ETH_MIN = web3.utils.toWei(process.env.MIN_ETH_SWEEP, 'ether');
-  const ETH_PRIORITY_FEE_GAS_GWEI = web3.utils.toWei(process.env.TX_MAX_PRIORITY_FEE, 'gwei');
-  const ETH_GAS_GWEI = web3.utils.toWei(process.env.TX_MAX_GWEI, 'gwei');
+  const ETH_MIN = web3.utils.toWei(CONFIG.MIN_ETH_SWEEP, 'ether');
+  const ETH_PRIORITY_FEE_GAS_GWEI = web3.utils.toWei(CONFIG.TX_MAX_PRIORITY_FEE, 'gwei');
+  const ETH_GAS_GWEI = web3.utils.toWei(CONFIG.TX_MAX_GWEI, 'gwei');
 
   if (Number(balance) > Number(ETH_MIN)) {	// sweep!
     let transfer_amount = Number(balance) - ETH_GAS_GWEI * 21000 - ETH_PRIORITY_FEE_GAS_GWEI;
@@ -26,16 +28,16 @@ async function main() {
     // Creating the transaction object
     const tx = {
         from: signer.address,
-        to: process.env.WALLET_DESTINATION,
+        to: CONFIG.WALLET_DESTINATION,
         value: transfer_amount,
         gas: 21000,
         nonce: Number(nonce),
         maxPriorityFeePerGas: ETH_PRIORITY_FEE_GAS_GWEI,
         maxFeePerGas: ETH_GAS_GWEI,
-        chainId: process.env.CHAIN_ID
+        chainId: CONFIG.CHAIN_ID
     };
     signedTx = await web3.eth.accounts.signTransaction(tx, signer.privateKey);
-    console.log("Sweeping: " + transfer_amount + "\nFrom: " + signer.address +"\nTo: " + process.env.WALLET_DESTINATION);
+    console.log("Sweeping: " + web3.utils.fromWei(transfer_amount, 'ether') + " ETH\nFrom: " + signer.address +"\nTo: " + CONFIG.WALLET_DESTINATION);
     // Sending the transaction to the network
     const receipt = await web3.eth
     .sendSignedTransaction(signedTx.rawTransaction)
@@ -48,7 +50,8 @@ async function main() {
     });;
     // The transaction is now on chain!
     console.log(`Mined in block ${receipt.blockNumber}`);
+  } else {
+    console.log("Balance below threshold - transaction not needed");
   }
 }
-require("dotenv").config();
 main();
